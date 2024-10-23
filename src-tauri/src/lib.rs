31e4386cce532d8508d2698b9130df9b6e35cedf
tinlizard <will.ben.weeks@gmail.com>
2024-10-23 22:42:44 +0200

    added display of file directory in navbar

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,6 +1,7 @@
 use std::fs::File;
 use std::path::Path;
 use dirs::desktop_dir;
+use std::fs::read_dir;
 
 #[derive(Debug, thiserror::Error)]
 enum Error {
@@ -37,11 +38,42 @@ fn create_file(file_name: &str) -> Result<String, Error> {
     Ok("File created successfully.".to_string())
 }
 
+#[tauri::command]
+fn get_all_files() -> Vec<String> {
+  let mut all_files = vec!["".to_string()];
+
+  match desktop_dir() {
+    Some(desktop_path) => {
+      let string_path = desktop_path.into_os_string().into_string();
+      let file_string = format!("{}{}", string_path.unwrap(), "/");
+
+      match read_dir(file_string) {
+        Ok(files) =>{
+          for file in files {
+            match file {
+              Ok(file) => {
+                all_files.push(file.path().into_os_string().into_string().expect("Error in expect"));
+                for file in &mut all_files {
+                  file.push('\n');
+                }
+              },
+              Err(e) => eprintln!("Error checking files in directory: {}",e)
+            }
+          }
+        },
+        Err(e) => eprintln!("Error: {}",e)
+      } 
+    },
+    None => println!("Couldnt find directory")
+  }
+  all_files
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![create_file])
+        .invoke_handler(tauri::generate_handler![create_file,get_all_files])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/navbar.tsx
+++ src/navbar.tsx
@@ -1,15 +1,30 @@
 import { invoke } from "@tauri-apps/api/core";
 import './navbar.css'
+import { useEffect, useState } from "react";
 
 interface Input {
     input: string,
 };
 
 export default function Navbar({input}: Input) {
+    const [fileNames,setFileNames] = useState<any>("")
+
+    async function getFiles() {
+        const files = await invoke('get_all_files')
+        setFileNames(files)
+    }
+
+    useEffect(()=>{
+        getFiles()
+        console.log(typeof(fileNames))
+        console.log(fileNames)
+    },[])
+
     return(
         <div className="navbar">
             <ul>
                 <li><button onClick={()=>invoke('create_file', {fileName: input})}>Create file</button></li>
+                <li>{fileNames}</li>
             </ul>
         </div>
     )
